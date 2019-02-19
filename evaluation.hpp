7ef31164f29c0a6e9e253708ba59c5be8fe1aef3
Antoine Gélin <antoine.gelin@edu.esiee.fr>
2019-02-19 13:15:36 +0100

    fix

---
+++ evaluation.hpp
@@ -0,0 +1,8 @@
+#ifndef EVALUATION_H
+#define EVALUATION_H
+
+#include "move.hpp"
+
+int evaluate(move m);
+int hasWon(U64 bitboard);
+#endif
---
+++ test_eval.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include "Board.hpp"
+#include "move.hpp"
+#include "move_tree.hpp"
+
+int main(){
+
+/*00000000
+  01010101
+  10101010
+  01010101
+  10101010
+  00000000
+  10101010
+  01010101
+
+  00000000
+  10101010
+  01010101
+  10101010
+  01010101
+  00000000
+  01010101
+  10101010
+
+  0000000001010101101010100101010110101010000000001010101001010101
+
+  0000000010101010010101011010101001010101000000000101010110101010*/	
+
+	Board board1{0x55AA55AA00AA55ULL,0xAA55AA550055AAULL};	
+	
+	std::cout << "\nBoard=================================\n" << std::endl;
+	board1.print_board();
+
+	move mv = {
+		board1,
+		62,
+		WHITE
+	};
+
+	node last;
+	last.m = mv;
+	
+	std::cout << "Getting best move..." << std::endl;
+
+	move best_move; 
+	get_best_move(&last, 5, &best_move);
+
+	std::cout << "=================Best move==============\n" << std::endl;
+	board_after_move(best_move).print_board();
+
+}
